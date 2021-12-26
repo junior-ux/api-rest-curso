b988945e30cd816e5bbb6d80e74f3b92ed9fc79d
junior-ux <55417250+junior-ux@users.noreply.github.com>
2021-12-26 13:33:20 -0300

    configurando o server

---
+++ server.js
@@ -0,0 +1,7 @@
+ const { prototype } = require("events");
+ const http = require("http");
+ const app = require('./app');
+ const port = process.env.PORT || 3000;
+ const server = http.createServer(app);
+
+ server.listen(port);
